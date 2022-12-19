6b957d34067ebf757c03a2882dec323fea80624b
andrea-putzu <106688558+andrea-putzu@users.noreply.github.com>
2022-12-19 17:19:35 +0100

    Release uat (#29)
    * Feature/selc 1699 (#26)
    
    * add in-cluster-app serviceAccount
    
    * set io.kubernetes.client.util logging level to DEBUG
    
    * set io.kubernetes.client logging level to DEBUG
    
    * remove in-cluster-app service account
    
    * add in-cluster-app serviceAccount
    
    * Fix insitutions retrieval (#27)
    
    Fix error in institutions and products retrieval, added new PSP to institutionType and modified ProductStatus to handle new TESTING and PHASE_OUT statuses, and changed previous ProductStatus to ProductOnboardingStatus
    
    Co-authored-by: Rodolfo Viti <62432865+rodoviti@users.noreply.github.com>
    Co-authored-by: Kevin Silva <84286727+KevinSi96@users.noreply.github.com>

--- connector-api/src/main/java/it/pagopa/selfcare/external_api/model/onboarding/InstitutionType.java
+++ connector-api/src/main/java/it/pagopa/selfcare/external_api/model/onboarding/InstitutionType.java
@@ -4,5 +4,6 @@ public enum InstitutionType {
     PA,
     GSP,
     PT,
+    PSP,
     SCP
 }
--- connector-api/src/main/java/it/pagopa/selfcare/external_api/model/product/Product.java
+++ connector-api/src/main/java/it/pagopa/selfcare/external_api/model/product/Product.java
@@ -23,6 +23,7 @@ public class Product {
     private OffsetDateTime activatedAt;
     private boolean authorized;
     private String userRole;
+    private ProductOnboardingStatus productOnboardingStatus;
     private ProductStatus status;
     private String identityTokenAudience;
     private EnumMap<PartyRole, ProductRoleInfo> roleMappings;
