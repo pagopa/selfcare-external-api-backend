fe7904a3db357175cec1426276ea70ef0a7232d1
VALUETEAM\MannaSa <salvatore11.manna@emea.nttdata.com>
2023-02-03 13:11:26 +0100

    Added new api call to web level OnboardingController

---
+++ web/src/main/java/it/pagopa/selfcare/external_api/web/model/onboarding/GeographicTaxonomyDto.java
@@ -0,0 +1,20 @@
+package it.pagopa.selfcare.external_api.web.model.onboarding;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.validation.constraints.NotBlank;
+
+public class GeographicTaxonomyDto {
+
+    @ApiModelProperty(value = "${swagger.external_api.geographicTaxonomy.model.code}", required = true)
+    @JsonProperty(required = true)
+    @NotBlank
+    private String code;
+
+    @ApiModelProperty(value = "${swagger.external_api.geographicTaxonomy.model.desc}", required = true)
+    @JsonProperty(required = true)
+    @NotBlank
+    private String desc;
+
+}
