e507b7aa721684905cb2dbe45f26daedab8f9056
VALUETEAM\MannaSa <salvatore11.manna@emea.nttdata.com>
2023-02-06 14:53:08 +0100

    Modified swagger placeholders

--- web/src/main/java/it/pagopa/selfcare/external_api/web/model/institutions/GeographicTaxonomyDto.java
+++ web/src/main/java/it/pagopa/selfcare/external_api/web/model/institutions/GeographicTaxonomyDto.java
@@ -8,12 +8,12 @@ import javax.validation.constraints.NotBlank;
 
 @Data
 public class GeographicTaxonomyDto {
-    @ApiModelProperty(value = "${swagger.dashboard.geographicTaxonomy.model.code}", required = true)
+    @ApiModelProperty(value = "${swagger.external_api.geographicTaxonomy.model.code}", required = true)
     @JsonProperty(required = true)
     @NotBlank
     private String code;
 
-    @ApiModelProperty(value = "${swagger.dashboard.geographicTaxonomy.model.desc}", required = true)
+    @ApiModelProperty(value = "${swagger.external_api.geographicTaxonomy.model.desc}", required = true)
     @JsonProperty(required = true)
     @NotBlank
     private String desc;
