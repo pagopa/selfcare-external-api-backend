23fd38b54e04dc55bf64daa6102a7aa37f1953b2
gianmarcoplutino <119858159+gianmarcoplutino@users.noreply.github.com>
2023-03-13 11:05:46 +0100

    Feature/SELC-2074 (#57)

--- web/src/main/java/it/pagopa/selfcare/external_api/web/model/onboarding/ImportContractDto.java
+++ web/src/main/java/it/pagopa/selfcare/external_api/web/model/onboarding/ImportContractDto.java
@@ -2,8 +2,11 @@ package it.pagopa.selfcare.external_api.web.model.onboarding;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.OffsetDateTime;
 
 @Data
 public class ImportContractDto {
@@ -19,5 +22,10 @@ public class ImportContractDto {
     @ApiModelProperty(value = "${swagger.external_api.importContract.model.contractType}", required = true)
     @NotBlank
     private String contractType;
+
+    @ApiModelProperty(value = "${swagger.external_api.institutions.model.onboardingDate}", required = true)
+    @NotNull
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private OffsetDateTime onboardingDate;
 
 }
