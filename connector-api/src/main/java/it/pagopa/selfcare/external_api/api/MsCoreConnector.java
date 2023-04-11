a340a8af10281d32ead964ba3fc99ea97a7c8130
bgalgamu <kevin.si1396@gmail.com>
2023-04-11 14:18:25 +0200

    remove api

--- connector-api/src/main/java/it/pagopa/selfcare/external_api/api/MsCoreConnector.java
+++ connector-api/src/main/java/it/pagopa/selfcare/external_api/api/MsCoreConnector.java
@@ -1,10 +1,7 @@
 package it.pagopa.selfcare.external_api.api;
 
-import it.pagopa.selfcare.external_api.model.institutions.Institution;
 import it.pagopa.selfcare.external_api.model.pnpg.CreatePnPgInstitution;
 
 public interface MsCoreConnector {
     String createPnPgInstitution(CreatePnPgInstitution request);
-
-    Institution getInstitutionByExternalId(String externalId);
 }
