98a25f0ed4af67fe8790bdf936dc6181283a058a
bgalgamu <kevin.si1396@gmail.com>
2023-09-22 00:19:02 +0200

    add api to retrieve user's specific onboarded ProductDetails

--- core/src/main/java/it/pagopa/selfcare/external_api/core/UserService.java
+++ core/src/main/java/it/pagopa/selfcare/external_api/core/UserService.java
@@ -1,9 +1,12 @@
 package it.pagopa.selfcare.external_api.core;
 
+import it.pagopa.selfcare.external_api.model.user.UserDetailsWrapper;
 import it.pagopa.selfcare.external_api.model.user.UserInfoWrapper;
 
 public interface UserService {
 
     UserInfoWrapper getUserInfo(String fiscalCode);
+
+    UserDetailsWrapper getUserOnboardedProductDetails(String userId, String institutionId, String productId);
 
 }
