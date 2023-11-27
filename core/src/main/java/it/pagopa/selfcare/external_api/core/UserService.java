f5c5936ea4fbbec207001ccc0b15b092d4578a5f
pierpaolodidato89 <137791912+pierpaolodidato89@users.noreply.github.com>
2023-11-27 09:09:00 +0100

    [SELC-3450] feat: added status and user email into getUserInfo API (#152)
    * Added status and user email to getUserInfo API
    
    * Added unit test for getUserInfo
    
    * Update unit tests about UserService
    
    ---------
    
    Co-authored-by: pierpaolo.didato@emeal.nttdata.com <Aiap1955?^@#>

--- core/src/main/java/it/pagopa/selfcare/external_api/core/UserService.java
+++ core/src/main/java/it/pagopa/selfcare/external_api/core/UserService.java
@@ -1,11 +1,15 @@
 package it.pagopa.selfcare.external_api.core;
 
+import it.pagopa.selfcare.external_api.model.user.RelationshipState;
 import it.pagopa.selfcare.external_api.model.user.UserDetailsWrapper;
 import it.pagopa.selfcare.external_api.model.user.UserInfoWrapper;
 
+import java.util.List;
+import java.util.Set;
+
 public interface UserService {
 
-    UserInfoWrapper getUserInfo(String fiscalCode);
+    UserInfoWrapper getUserInfo(String fiscalCode, List<RelationshipState> userStatuses);
 
     UserDetailsWrapper getUserOnboardedProductDetails(String userId, String institutionId, String productId);
 
