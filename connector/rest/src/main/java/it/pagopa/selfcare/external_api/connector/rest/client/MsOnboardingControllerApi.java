e06211cb9e428f301370d0703790c66f4edaf9ec
pierpaolo.didato@emeal.nttdata.com <Aiap1955?^@#>
2024-01-31 17:35:39 +0100

    Update rest client configuration for ms-onboarding

--- connector/rest/src/main/java/it/pagopa/selfcare/external_api/connector/rest/client/MsOnboardingControllerApi.java
+++ connector/rest/src/main/java/it/pagopa/selfcare/external_api/connector/rest/client/MsOnboardingControllerApi.java
@@ -5,6 +5,6 @@ import it.pagopa.selfcare.onboarding.generated.openapi.v1.api.TokenControllerApi
 import org.springframework.cloud.openfeign.FeignClient;
 
 
-@FeignClient(name = "${rest-client.ms-onboarding-controller-api.serviceCode}", url = "${rest-client.ms-onboarding.base-url}")
+@FeignClient(name = "${rest-client.ms-onboarding-api.serviceCode}", url = "${rest-client.ms-onboarding.base-url}")
 public interface MsOnboardingControllerApi extends OnboardingControllerApi {
 }
