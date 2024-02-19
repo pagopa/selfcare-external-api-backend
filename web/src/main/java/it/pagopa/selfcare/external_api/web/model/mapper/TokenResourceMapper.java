3b1af826506c2021f68c6f328b322a9e5e989c14
manuraf <manuel.rafeli@pagopa.it>
2024-02-19 14:12:10 +0100

    avoid nullpointer

--- web/src/main/java/it/pagopa/selfcare/external_api/web/model/mapper/TokenResourceMapper.java
+++ web/src/main/java/it/pagopa/selfcare/external_api/web/model/mapper/TokenResourceMapper.java
@@ -10,6 +10,7 @@ import org.mapstruct.Named;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring", imports = List.class)
@@ -23,17 +24,23 @@ public interface TokenResourceMapper {
         List<LegalsResource> legalsResponses = new ArrayList<>();
         for(UserProducts user: tokenOnboardedUsers.getOnboardedUsers()){
             List<LegalsResource> list = user.getBindings().stream()
-                    .filter(userBinding -> tokenOnboardedUsers.getInstitutionId().equalsIgnoreCase(userBinding.getInstitutionId()))
+                    .filter(userBinding -> Optional.ofNullable(tokenOnboardedUsers.getInstitutionId())
+                            .map(value -> value.equalsIgnoreCase(userBinding.getInstitutionId()))
+                            .orElse(false))
                     .flatMap(userBinding -> userBinding.getProducts().stream())
-                    .filter(onboardedProduct -> tokenOnboardedUsers.getProductId().equalsIgnoreCase(onboardedProduct.getProductId()))
-                    .filter(onboardedProduct -> tokenOnboardedUsers.getId().equals(onboardedProduct.getTokenId()))
+                    .filter(onboardedProduct -> Optional.ofNullable(tokenOnboardedUsers.getProductId())
+                            .map(value -> value.equalsIgnoreCase(onboardedProduct.getProductId()))
+                            .orElse(false))
+                    .filter(onboardedProduct -> Optional.ofNullable(tokenOnboardedUsers.getId())
+                            .map(value -> value.equals(onboardedProduct.getTokenId()))
+                            .orElse(false))
                     .map(product -> {
                         LegalsResource legalsResponse = new LegalsResource();
                         legalsResponse.setPartyId(user.getId());
                         legalsResponse.setEnv(product.getEnv());
                         legalsResponse.setRole(product.getRole());
                         return legalsResponse;
-                    }).collect(Collectors.toList());
+                    }).toList();
             legalsResponses.addAll(list);
         }
         return legalsResponses;
