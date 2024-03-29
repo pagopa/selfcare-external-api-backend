f5cc86035e0ef019fd107b06249854532abf53ce
manuraf <manuel.rafeli@pagopa.it>
2024-03-29 12:42:17 +0100

    rename module

--- infra/container_apps/main.tf
+++ infra/container_apps/main.tf
@@ -8,7 +8,7 @@ provider "azurerm" {
   features {}
 }
 
-module "container_app_dashboard_backend" {
+module "container_app_ext_api_backend" {
   source = "github.com/pagopa/selfcare-commons//infra/terraform-modules/container_app_microservice?ref=main"
 
   is_pnpg = var.is_pnpg
@@ -20,6 +20,7 @@ module "container_app_dashboard_backend" {
   image_tag          = var.image_tag
   app_settings       = var.app_settings
   secrets_names      = var.secrets_names
+  workload_profile_name = var.workload_profile_name
 
   tags = var.tags
 }
