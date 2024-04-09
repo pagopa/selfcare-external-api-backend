b24f8f21c2af2a9e6357a285472a222cf6aecaa7
manuraf <manuel.rafeli@pagopa.it>
2024-04-09 16:00:35 +0200

    container_app_environment_name

--- infra/container_apps/main.tf
+++ infra/container_apps/main.tf
@@ -16,6 +16,7 @@ module "container_app_ext_api_backend" {
   env_short          = var.env_short
   container_app      = var.container_app
   container_app_name = "ext-api-backend"
+  container_app_environment_name = local.container_app_environment_name
   image_name         = "selfcare-external-api-backend"
   image_tag          = var.image_tag
   app_settings       = var.app_settings
