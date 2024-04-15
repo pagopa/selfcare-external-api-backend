6e4274f9ae29706f92a9f81ebc4d9d23001fa5f8
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2024-04-15 10:06:26 +0200

    [SELC-4640] Add tests for UserMsConnectorImpl

--- connector/rest/src/test/java/it/pagopa/selfcare/external_api/connector/rest/UserMsConnectorImplTest.java
+++ connector/rest/src/test/java/it/pagopa/selfcare/external_api/connector/rest/UserMsConnectorImplTest.java
@@ -4,7 +4,10 @@ import it.pagopa.selfcare.commons.base.security.PartyRole;
 import it.pagopa.selfcare.external_api.connector.rest.client.MsUserApiRestClient;
 import it.pagopa.selfcare.external_api.connector.rest.mapper.UserMapper;
 import it.pagopa.selfcare.external_api.connector.rest.mapper.UserMapperImpl;
-import it.pagopa.selfcare.user.generated.openapi.v1.dto.UserInstitutionResponse;
+import it.pagopa.selfcare.external_api.connector.rest.mapper.UserResourceMapper;
+import it.pagopa.selfcare.external_api.model.institutions.Institution;
+import it.pagopa.selfcare.external_api.model.user.UserToOnboard;
+import it.pagopa.selfcare.user.generated.openapi.v1.dto.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -28,6 +31,12 @@ class UserMsConnectorImplTest {
     @Mock
     private MsUserApiRestClient msUserApiRestClient;
 
+    @Mock
+    private UserResourceMapper userResourceMapper;
+
+    @Mock
+    private UserMapper userMapper;
+
     @Spy
     UserMapper mapper = new UserMapperImpl();
 
@@ -47,6 +56,114 @@ class UserMsConnectorImplTest {
         userMsConnector.getUsersInstitutions(null, null, null, null, null, null, null, null);
 
         verify(msUserApiRestClient, times(1))._usersGet(null, null, null, null, Collections.emptyList(), null, null, null);
+    }
+
+    @Test
+    void createUser_happyPath() {
+        Institution institution = new Institution();
+        institution.setId("institutionId");
+        institution.setDescription("description");
+        institution.setParentDescription("parentDescription");
+
+        UserToOnboard userToOnboard = new UserToOnboard();
+        userToOnboard.setId("userId");
+
+        when(userResourceMapper.toUser(any(UserToOnboard.class))).thenReturn(new User());
+        when(msUserApiRestClient._usersPost(any(CreateUserDto.class))).thenReturn(new ResponseEntity<>("userId", HttpStatus.OK));
+
+        String userId = userMsConnector.createUser(institution, "productId", "MANAGER", List.of("productRole"), userToOnboard);
+
+        verify(msUserApiRestClient, times(1))._usersPost(any(CreateUserDto.class));
+        verify(userResourceMapper, times(1)).toUser(any(UserToOnboard.class));
+        assert userId.equals("userId");
+    }
+
+    @Test
+    void createUser_nullUserToOnboard() {
+        Institution institution = new Institution();
+        institution.setId("institutionId");
+        institution.setDescription("description");
+        institution.setParentDescription("parentDescription");
+
+        when(msUserApiRestClient._usersPost(any(CreateUserDto.class))).thenReturn(new ResponseEntity<>("userId", HttpStatus.OK));
+
+        String userId = userMsConnector.createUser(institution, "productId", "MANAGER", List.of("productRole"), null);
+
+        verify(msUserApiRestClient, times(1))._usersPost(any(CreateUserDto.class));
+        assert userId.equals("userId");
+    }
+
+    @Test
+    void addUserRole_happyPath() {
+        Institution institution = new Institution();
+        institution.setId("institutionId");
+        institution.setDescription("description");
+        institution.setParentDescription("parentDescription");
+
+        when(msUserApiRestClient._usersUserIdPost(anyString(), any(AddUserRoleDto.class))).thenReturn(new ResponseEntity<>(HttpStatus.OK));
+
+        String returnedUserId = userMsConnector.addUserRole("userId", institution, "productId", "MANAGER", List.of("productRole"));
+
+        verify(msUserApiRestClient, times(1))._usersUserIdPost(anyString(), any(AddUserRoleDto.class));
+        assert returnedUserId.equals("userId");
+    }
+
+    @Test
+    void addUserRole_nullProductRoles() {
+        Institution institution = new Institution();
+        institution.setId("institutionId");
+        institution.setDescription("description");
+        institution.setParentDescription("parentDescription");
+
+        when(msUserApiRestClient._usersUserIdPost(anyString(), any(AddUserRoleDto.class))).thenReturn(new ResponseEntity<>(HttpStatus.OK));
+
+        String returnedUserId = userMsConnector.addUserRole("userId", institution, "productId", "MANAGER", null);
+
+        verify(msUserApiRestClient, times(1))._usersUserIdPost(anyString(), any(AddUserRoleDto.class));
+        assert returnedUserId.equals("userId");
+    }
+
+    @Test
+    void searchUserByExternalId_happyPath() {
+        UserDetailResponse userDetailResponse = new UserDetailResponse();
+        userDetailResponse.setId("userId");
+
+        when(msUserApiRestClient._usersSearchPost(any(), any(SearchUserDto.class))).thenReturn(new ResponseEntity<>(userDetailResponse, HttpStatus.OK));
+
+        it.pagopa.selfcare.external_api.model.user.User user = userMsConnector.searchUserByExternalId("fiscalCode");
+
+        verify(msUserApiRestClient, times(1))._usersSearchPost(any(), any(SearchUserDto.class));
+        assert user != null;
+    }
+
+    @Test
+    void searchUserByExternalId_nullFiscalCode() {
+        UserDetailResponse userDetailResponse = new UserDetailResponse();
+        userDetailResponse.setId("userId");
+
+        when(msUserApiRestClient._usersSearchPost(any(), any(SearchUserDto.class))).thenReturn(new ResponseEntity<>(userDetailResponse, HttpStatus.OK));
+        when(userMapper.toUserFromUserDetailResponse(any(UserDetailResponse.class))).thenReturn(new it.pagopa.selfcare.external_api.model.user.User());
+
+        it.pagopa.selfcare.external_api.model.user.User user = userMsConnector.searchUserByExternalId(null);
+
+        verify(msUserApiRestClient, times(1))._usersSearchPost(any(), any(SearchUserDto.class));
+        verify(userMapper, times(1)).toUserFromUserDetailResponse(any(UserDetailResponse.class));
+        assert user != null;
+    }
+
+    @Test
+    void searchUserByExternalId_emptyFiscalCode() {
+        UserDetailResponse userDetailResponse = new UserDetailResponse();
+        userDetailResponse.setId("userId");
+
+        when(msUserApiRestClient._usersSearchPost(any(), any(SearchUserDto.class))).thenReturn(new ResponseEntity<>(userDetailResponse, HttpStatus.OK));
+        when(userMapper.toUserFromUserDetailResponse(any(UserDetailResponse.class))).thenReturn(new it.pagopa.selfcare.external_api.model.user.User());
+
+        it.pagopa.selfcare.external_api.model.user.User user = userMsConnector.searchUserByExternalId("");
+
+        verify(msUserApiRestClient, times(1))._usersSearchPost(any(), any(SearchUserDto.class));
+        verify(userMapper, times(1)).toUserFromUserDetailResponse(any(UserDetailResponse.class));
+        assert user != null;
     }
 
 }
