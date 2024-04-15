aa85c76559852685e68a3493f7db0c1cfc082d75
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2024-04-15 14:52:12 +0200

    [SELC-4640] Added tests for onboard users

--- web/src/test/java/it/pagopa/selfcare/external_api/web/controller/OnboardingV2ControllerTest.java
+++ web/src/test/java/it/pagopa/selfcare/external_api/web/controller/OnboardingV2ControllerTest.java
@@ -1,19 +1,34 @@
 package it.pagopa.selfcare.external_api.web.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import it.pagopa.selfcare.commons.base.security.SelfCareUser;
 import it.pagopa.selfcare.external_api.core.OnboardingService;
+import it.pagopa.selfcare.external_api.model.institutions.Institution;
 import it.pagopa.selfcare.external_api.model.onboarding.OnboardingData;
+import it.pagopa.selfcare.external_api.model.onboarding.OnboardingUsersRequest;
+import it.pagopa.selfcare.external_api.model.user.RelationshipInfo;
+import it.pagopa.selfcare.external_api.model.user.UserToOnboard;
 import it.pagopa.selfcare.external_api.web.config.WebTestConfig;
+import it.pagopa.selfcare.external_api.web.model.mapper.OnboardingResourceMapper;
 import it.pagopa.selfcare.external_api.web.model.mapper.OnboardingResourceMapperImpl;
+import it.pagopa.selfcare.external_api.web.model.onboarding.OnboardingInstitutionUsersRequest;
+import it.pagopa.selfcare.external_api.web.model.user.Person;
+import it.pagopa.selfcare.external_api.web.model.user.UserDto;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.io.Resource;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.List;
+import java.util.UUID;
 
 import static org.hamcrest.Matchers.emptyString;
 import static org.mockito.ArgumentMatchers.any;
@@ -23,7 +38,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(value = {OnboardingV2Controller.class}, excludeAutoConfiguration = SecurityAutoConfiguration.class)
-@ContextConfiguration(classes = {OnboardingV2Controller.class, WebTestConfig.class, OnboardingResourceMapperImpl.class})
+@ContextConfiguration(classes = {OnboardingV2Controller.class, WebTestConfig.class})
 public class OnboardingV2ControllerTest {
 
     private static final String BASE_URL = "/v2/onboarding";
@@ -65,6 +80,31 @@ public class OnboardingV2ControllerTest {
         // then
         verify(onboardingServiceMock, times(1))
                 .oldContractOnboardingV2(any(OnboardingData.class));
+        verifyNoMoreInteractions(onboardingServiceMock);
+    }
+
+    @Test
+    void onboardingInstitutionUsers() throws Exception {
+        OnboardingInstitutionUsersRequest request = new OnboardingInstitutionUsersRequest();
+        request.setInstitutionTaxCode("taxCode");
+        request.setProductId("productCode");
+        request.setUsers(List.of(new Person()));
+
+
+        when(onboardingServiceMock.onboardingUsers(any(OnboardingUsersRequest.class), anyString(), anyString())).thenReturn(List.of(new RelationshipInfo()));
+        SelfCareUser selfCareUser = SelfCareUser.builder("id").name("nome").surname("cognome").build();
+        Authentication authentication = mock(Authentication.class);
+        when(authentication.getPrincipal()).thenReturn(selfCareUser);
+
+        mvc.perform(MockMvcRequestBuilders
+                        .post(BASE_URL + "/users")
+                        .principal(authentication)
+                        .content(new ObjectMapper().writeValueAsString(request))
+                        .contentType(APPLICATION_JSON_VALUE)
+                        .accept(APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk());
+
+        verify(onboardingServiceMock, times(1)).onboardingUsers(any(OnboardingUsersRequest.class), anyString(), anyString());
         verifyNoMoreInteractions(onboardingServiceMock);
     }
 }
