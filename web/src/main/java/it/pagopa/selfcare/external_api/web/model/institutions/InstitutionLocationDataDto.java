fb23c2fa5c0181102397ee5004a2e5687455a151
andrea-putzu <106688558+andrea-putzu@users.noreply.github.com>
2024-04-29 17:26:44 +0200

    fix: Removing field mandatory for onboarding (#224)

--- web/src/main/java/it/pagopa/selfcare/external_api/web/model/institutions/InstitutionLocationDataDto.java
+++ web/src/main/java/it/pagopa/selfcare/external_api/web/model/institutions/InstitutionLocationDataDto.java
@@ -5,13 +5,13 @@ import lombok.Data;
 
 @Data
 public class InstitutionLocationDataDto {
-    @ApiModelProperty(value = "${swagger.external_api.institutions.model.city}", required = true)
+    @ApiModelProperty(value = "${swagger.external_api.institutions.model.city}")
     private String city;
 
-    @ApiModelProperty(value = "${swagger.external_api.institutions.model.county}", required = true)
+    @ApiModelProperty(value = "${swagger.external_api.institutions.model.county}")
     private String county;
 
-    @ApiModelProperty(value = "${swagger.external_api.institutions.model.country}", required = true)
+    @ApiModelProperty(value = "${swagger.external_api.institutions.model.country}")
     private String country;
 
 }
