3ca2e15fcf5abbfc6d40ce2ffaaa9997470f0bf0
Manuel Rafeli <manuel.rafeli@pagopa.it>
2024-05-09 14:49:46 +0200

    [SELC-4849] refactor: getOnboardedInstitutionsDetails for setting correct product status (#233)

--- connector-api/src/main/java/it/pagopa/selfcare/external_api/model/user/RelationshipState.java
+++ connector-api/src/main/java/it/pagopa/selfcare/external_api/model/user/RelationshipState.java
@@ -1,10 +1,10 @@
 package it.pagopa.selfcare.external_api.model.user;
 
 public enum RelationshipState {
-    PENDING,
     ACTIVE,
     SUSPENDED,
     DELETED,
     REJECTED,
+    PENDING,
     TOBEVALIDATED;
 }
