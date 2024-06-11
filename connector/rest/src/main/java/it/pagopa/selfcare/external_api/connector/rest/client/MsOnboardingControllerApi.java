d1228d2dd7a6cc7ac4e7e2dffd7dbbad2149146c
Giulia Tremolada <giulia.tremolada@nttdata.com>
2024-06-11 16:51:58 +0200

    align openapi

--- connector/rest/src/main/java/it/pagopa/selfcare/external_api/connector/rest/client/MsOnboardingControllerApi.java
+++ connector/rest/src/main/java/it/pagopa/selfcare/external_api/connector/rest/client/MsOnboardingControllerApi.java
@@ -1,9 +1,9 @@
 package it.pagopa.selfcare.external_api.connector.rest.client;
 
-import it.pagopa.selfcare.onboarding.generated.openapi.v1.api.OnboardingApi;
+import it.pagopa.selfcare.onboarding.generated.openapi.v1.api.OnboardingControllerApi;
 import org.springframework.cloud.openfeign.FeignClient;
 
 
 @FeignClient(name = "${rest-client.ms-onboarding-api.serviceCode}", url = "${rest-client.ms-onboarding.base-url}")
-public interface MsOnboardingControllerApi extends OnboardingApi {
+public interface MsOnboardingControllerApi extends OnboardingControllerApi {
 }
