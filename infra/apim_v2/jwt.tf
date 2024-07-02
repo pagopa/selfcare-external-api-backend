ac918531e243466816fbc29aa97cf286c2e54b9a
christian-calabrese <christian.calabrese@pagopa.it>
2024-07-02 18:19:36 +0200

    fix: added pnpg certificate to v2 apims

--- infra/apim_v2/jwt.tf
+++ infra/apim_v2/jwt.tf
@@ -10,3 +10,16 @@ resource "azurerm_api_management_certificate" "jwt_certificate" {
   resource_group_name = azurerm_resource_group.rg_api.name
   data                = pkcs12_from_pem.jwt_pkcs12.result
 }
+
+resource "pkcs12_from_pem" "jwt_pkcs12_pnpg" {
+  password        = ""
+  cert_pem        = data.azurerm_key_vault_secret.jwt_certificate_data_pem_pnpg.value
+  private_key_pem = data.azurerm_key_vault_secret.jwt_private_key_pem_pnpg.value
+}
+
+resource "azurerm_api_management_certificate" "jwt_certificate_pnpg" {
+  name                = "jwt-pnpg-spid-crt"
+  api_management_name = data.azurerm_api_management.api_management_core.name
+  resource_group_name = data.azurerm_api_management.api_management_core.resource_group_name
+  data                = pkcs12_from_pem.jwt_pkcs12.result
+}
