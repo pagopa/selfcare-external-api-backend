5254ea77c0276a310a57c464e4453d03f8cce6e7
manuraf <manuel.rafeli@pagopa.it>
2024-07-10 14:28:31 +0200

    remove developer portal

--- infra/apim_v2/developer.tf
+++ infra/apim_v2/developer.tf
@@ -5,51 +5,6 @@ locals {
   checkout_cdn_name      = "${local.project}-checkout-cdn-endpoint"
 }
 
-resource "null_resource" "download_apim_external_api_v1" {
-  triggers = {
-    file_sha1 = filesha1("./api/ms_external_api/v1/open-api.yml.tpl")
-  }
-
-  depends_on = [module.apim_external_api_ms_v1]
-
-  provisioner "local-exec" {
-    command = <<EOT
-      mkdir -p "${path.module}/.terraform/tmp/env/${var.env}/developer/external"
-      az rest \
-        --method get \
-        --url https://management.azure.com/subscriptions/${data.azurerm_subscription.current.subscription_id}/resourceGroups/${module.apim.resource_group_name}/providers/Microsoft.ApiManagement/service/${module.apim.name}/apis/${module.apim_external_api_ms_v1.name} \
-        --url-parameters api-version=${local.azure_apim_api_version} export=true format=openapi \
- 	      --output-file ${path.module}/.terraform/tmp/env/${var.env}/developer/external/ms-external-api-v1.yaml
-    EOT
-  }
-}
-
-resource "null_resource" "upload_developer_external_api_v1" {
-  triggers = {
-    file_sha1 = filesha1("./api/ms_external_api/v1/open-api.yml.tpl")
-  }
-
-  depends_on = [null_resource.download_apim_external_api_v1]
-
-  provisioner "local-exec" {
-    command = <<EOT
-              az storage blob upload \
-                --container '$web' \
-                --account-name ${replace(replace(local.checkout_cdn_name, "-cdn-endpoint", "-sa"), "-", "")} \
-                --account-key ${data.azurerm_storage_account.checkout.primary_access_key} \
-                --file "${path.module}/.terraform/tmp/env/${var.env}/developer/external/ms-external-api-v1.yaml" \
-                --overwrite true \
-                --name 'developer/external/v1/ms-external-api.yaml' &&
-              az cdn endpoint purge \
-                --resource-group ${data.azurerm_storage_account.checkout.resource_group_name} \
-                --name ${local.checkout_cdn_name} \
-                --profile-name ${replace(local.checkout_cdn_name, "-cdn-endpoint", "-cdn-profile")}  \
-                --content-paths "/developer/external/v1/ms-external-api.yaml" \
-                --no-wait
-          EOT
-  }
-}
-
 resource "null_resource" "upload_developer_index_v1" {
   triggers = {
     file_sha1 = filesha1("./env/${var.env}/developer/external/v1/index.html")
