6b0e62957e43653d6c90e512b069384f36df0b12
flaminiaScarciofolo <113031535+flaminiaScarciofolo@users.noreply.github.com>
2024-07-24 09:16:11 +0200

    [SELC-5243] Refactor APIs to retrieve UserData with role and productRoles (#288)

--- core/src/main/java/it/pagopa/selfcare/external_api/core/InstitutionService.java
+++ core/src/main/java/it/pagopa/selfcare/external_api/core/InstitutionService.java
@@ -5,12 +5,11 @@ import it.pagopa.selfcare.external_api.model.institutions.Institution;
 import it.pagopa.selfcare.external_api.model.institutions.SearchMode;
 import it.pagopa.selfcare.external_api.model.nationalRegistries.LegalVerification;
 import it.pagopa.selfcare.external_api.model.pnpg.CreatePnPgInstitution;
-import it.pagopa.selfcare.external_api.model.user.UserInfo;
+import it.pagopa.selfcare.external_api.model.user.UserProductResponse;
 import it.pagopa.selfcare.product.entity.Product;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 public interface InstitutionService {
@@ -18,7 +17,7 @@ public interface InstitutionService {
 
     List<Product> getInstitutionUserProductsV2(String institutionId, String userId);
 
-    Collection<UserInfo> getInstitutionProductUsersV2(String institutionId, String productId, String userId, Optional<Set<String>> productRoles, String xSelfCareUid);
+    Collection<UserProductResponse> getInstitutionProductUsersV2(String institutionId, String productId, String userId, Set<String> productRoles, String xSelfCareUid);
 
     List<GeographicTaxonomy> getGeographicTaxonomyList(String institutionId);
 
