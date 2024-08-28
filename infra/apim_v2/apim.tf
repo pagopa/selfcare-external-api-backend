8908b800335d15ebff81ce32b8b733cb8d79a7a7
manuraf <manuel.rafeli@pagopa.it>
2024-08-28 09:45:16 +0200

    feat: using common jwt policy

--- infra/apim_v2/apim.tf
+++ infra/apim_v2/apim.tf
@@ -222,20 +222,13 @@ module "apim_external_api_onboarding_io_v1" {
     basePath = "/onboarding-api/v1"
   })
 
-  xml_content = file("./api/base_policy.xml")
+  xml_content = templatefile("./api/jwt_base_policy.xml.tpl", {
+    API_DOMAIN                 = local.api_domain
+    KID                        = data.azurerm_key_vault_secret.jwt_kid.value
+    JWT_CERTIFICATE_THUMBPRINT = azurerm_api_management_certificate.jwt_certificate.thumbprint
+  })
 
   subscription_required = true
-
-  api_operation_policies = [
-    {
-      operation_id = "contractOnboardingUsingPOST"
-      xml_content = templatefile("./api/external-api-onboarding-io/v1/contractOnboarding_op_policy.xml.tpl", {
-        API_DOMAIN                 = local.api_domain
-        KID                        = data.azurerm_key_vault_secret.jwt_kid.value
-        JWT_CERTIFICATE_THUMBPRINT = azurerm_api_management_certificate.jwt_certificate.thumbprint
-      })
-    }
-  ]
 }
 
 resource "azurerm_api_management_api_version_set" "apim_external_api_ms" {
