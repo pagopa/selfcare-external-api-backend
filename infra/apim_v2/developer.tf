147a5bcf0fad59ef93afc1575a15cee17f287f4d
manuraf <manuel.rafeli@pagopa.it>
2024-09-16 14:58:32 +0200

    feat: upload index.html

--- infra/apim_v2/developer.tf
+++ infra/apim_v2/developer.tf
@@ -3,6 +3,7 @@ locals {
   service_name           = module.apim.name
   azure_apim_api_version = "2021-08-01"
   checkout_cdn_name      = "${local.project}-checkout-cdn-endpoint"
+  checkout_cdn_name_pnpg      = "${local.project}-weu-pnpg-checkout-cdn-endpoint"
 }
 
 resource "null_resource" "upload_developer_index_v2" {
@@ -72,6 +73,30 @@ resource "null_resource" "upload_internal_developer_index_v1" {
                 --name ${local.checkout_cdn_name} \
                 --profile-name ${replace(local.checkout_cdn_name, "-cdn-endpoint", "-cdn-profile")}  \
                 --content-paths "/developer/internal/v1/index.html" \
+                --no-wait
+          EOT
+  }
+}
+
+resource "null_resource" "upload_support_pnpg_developer_index_v1" {
+  triggers = {
+    file_sha1 = filesha1("./env/${var.env}/developer/support-pnpg/v1/index.html")
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+              az storage blob upload \
+                --container '$web' \
+                --account-name ${replace(replace(local.checkout_cdn_name_pnpg, "-cdn-endpoint", "-sa"), "-", "")} \
+                --account-key ${data.azurerm_storage_account.checkout_pnpg.primary_access_key} \
+                --file "./env/${var.env}/developer/support-pnpg/v1/index.html" \
+                --overwrite true \
+                --name 'developer/support-pnpg/v1/index.html' &&
+              az cdn endpoint purge \
+                --resource-group ${data.azurerm_storage_account.checkout_pnpg.resource_group_name} \
+                --name ${local.checkout_cdn_name_pnpg} \
+                --profile-name ${replace(local.checkout_cdn_name_pnpg, "-cdn-endpoint", "-cdn-profile")}  \
+                --content-paths "/developer/support-pnpg/v1/index.html" \
                 --no-wait
           EOT
   }
