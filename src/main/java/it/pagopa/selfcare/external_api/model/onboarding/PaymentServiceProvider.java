e9b814c1a1dbe1313735a1a8612ca57642c93e3e
gianmarcoplutino <gianmarco.plutino@pagopa.it>
2024-10-28 12:25:11 +0100

    feat: Added providerNames, contractType, contractId to PSPs' PaymentServiceProvider and added OnboardingImportProductDto

--- src/main/java/it/pagopa/selfcare/external_api/model/onboarding/PaymentServiceProvider.java
+++ src/main/java/it/pagopa/selfcare/external_api/model/onboarding/PaymentServiceProvider.java
@@ -2,13 +2,17 @@ package it.pagopa.selfcare.external_api.model.onboarding;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class PaymentServiceProvider {
 
-    private String abiCode;
-    private String businessRegisterNumber;
-    private String legalRegisterName;
-    private String legalRegisterNumber;
-    private Boolean vatNumberGroup;
-
+  private String abiCode;
+  private String businessRegisterNumber;
+  private String legalRegisterName;
+  private String legalRegisterNumber;
+  private Boolean vatNumberGroup;
+  private String contractType;
+  private String contractId;
+  private List<String> providerNames;
 }
