6764b2f1fee924823e7daddc13252109ed98200e
gianmarcoplutino <119858159+gianmarcoplutino@users.noreply.github.com>
2024-10-28 12:39:49 +0100

    [SELC - 5811] feat: Added providerNames, contractType, contractId to PSPs' PaymentS… (#454)

--- src/main/java/it/pagopa/selfcare/external_api/model/onboarding/PspDataDto.java
+++ src/main/java/it/pagopa/selfcare/external_api/model/onboarding/PspDataDto.java
@@ -7,38 +7,59 @@ import lombok.Data;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Data
 public class PspDataDto {
 
-    @ApiModelProperty(value = "${swagger.external_api.institutions.model.pspData.businessRegisterNumber}", required = true)
-    @JsonProperty(required = true)
-    @NotBlank
-    private String businessRegisterNumber;
+  @ApiModelProperty(
+      value = "${swagger.external_api.institutions.model.pspData.businessRegisterNumber}",
+      required = true)
+  @JsonProperty(required = true)
+  @NotBlank
+  private String businessRegisterNumber;
 
-    @ApiModelProperty(value = "${swagger.external_api.institutions.model.pspData.legalRegisterName}", required = true)
-    @JsonProperty(required = true)
-    @NotBlank
-    private String legalRegisterName;
+  @ApiModelProperty(
+      value = "${swagger.external_api.institutions.model.pspData.legalRegisterName}",
+      required = true)
+  @JsonProperty(required = true)
+  @NotBlank
+  private String legalRegisterName;
 
-    @ApiModelProperty(value = "${swagger.external_api.institutions.model.pspData.legalRegisterNumber}", required = true)
-    @JsonProperty(required = true)
-    @NotBlank
-    private String legalRegisterNumber;
+  @ApiModelProperty(
+      value = "${swagger.external_api.institutions.model.pspData.legalRegisterNumber}",
+      required = true)
+  @JsonProperty(required = true)
+  @NotBlank
+  private String legalRegisterNumber;
 
-    @ApiModelProperty(value = "${swagger.external_api.institutions.model.pspData.abiCode}", required = true)
-    @JsonProperty(required = true)
-    @NotBlank
-    private String abiCode;
+  @ApiModelProperty(
+      value = "${swagger.external_api.institutions.model.pspData.abiCode}",
+      required = true)
+  @JsonProperty(required = true)
+  @NotBlank
+  private String abiCode;
 
-    @ApiModelProperty(value = "${swagger.external_api.institutions.model.pspData.vatNumberGroup}", required = true)
-    @JsonProperty(required = true)
-    @NotNull
-    private Boolean vatNumberGroup;
+  @ApiModelProperty(
+      value = "${swagger.external_api.institutions.model.pspData.vatNumberGroup}",
+      required = true)
+  @JsonProperty(required = true)
+  @NotNull
+  private Boolean vatNumberGroup;
 
-    @ApiModelProperty(value = "${swagger.external_api.institutions.model.pspData.dpoData}", required = true)
-    @NotNull
-    @Valid
-    private DpoDataDto dpoData;
+  @ApiModelProperty(value = "${swagger.external_api.institutions.model.pspData.contractType}")
+  private String contractType;
 
+  @ApiModelProperty(value = "${swagger.external_api.institutions.model.pspData.contractId}")
+  private String contractId;
+
+  @ApiModelProperty(value = "${swagger.external_api.institutions.model.pspData.providerNames}")
+  private List<String> providerNames;
+
+  @ApiModelProperty(
+      value = "${swagger.external_api.institutions.model.pspData.dpoData}",
+      required = true)
+  @NotNull
+  @Valid
+  private DpoDataDto dpoData;
 }
