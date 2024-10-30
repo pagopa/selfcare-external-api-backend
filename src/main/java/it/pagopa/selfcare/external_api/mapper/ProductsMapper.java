febce55a375742904ecdb9cd36416f3e22f82503
Manuel Rafeli <manuel.rafeli@pagopa.it>
2024-10-30 14:21:25 +0100

    [SELC-5895] fix: refactor mapping contractTemplatePath and contractTemplateVersion (#469)

--- src/main/java/it/pagopa/selfcare/external_api/mapper/ProductsMapper.java
+++ src/main/java/it/pagopa/selfcare/external_api/mapper/ProductsMapper.java
@@ -2,6 +2,7 @@ package it.pagopa.selfcare.external_api.mapper;
 
 import it.pagopa.selfcare.external_api.model.product.ProductResource;
 import it.pagopa.selfcare.onboarding.common.PartyRole;
+import it.pagopa.selfcare.product.entity.ContractTemplate;
 import it.pagopa.selfcare.product.entity.Product;
 import it.pagopa.selfcare.product.entity.ProductRoleInfo;
 import org.mapstruct.Mapper;
@@ -10,11 +11,15 @@ import org.mapstruct.Named;
 
 import java.util.EnumMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 
 @Mapper(componentModel = "spring")
 public interface ProductsMapper {
 
     @Mapping(target = "roleMappings", expression = "java(toRoleMappings(model.getRoleMappings(institutionType)))")
+    @Mapping(target = "contractTemplatePath", expression = "java(toContractTemplatePath(model,institutionType))")
+    @Mapping(target = "contractTemplateVersion", expression = "java(toContractTemplateVersion(model,institutionType))")
     ProductResource toResource(Product model, String institutionType);
 
     @Named("toRoleMappings")
@@ -34,5 +39,23 @@ public interface ProductsMapper {
             result = null;
         }
         return result;
+    }
+
+    @Named("toContractTemplatePath")
+    default String toContractTemplatePath(Product model, String institutionType){
+        if(Objects.isNull(institutionType)) return null;
+        return Optional.ofNullable(model.getInstitutionContractMappings())
+                .map(map -> map.get(institutionType))
+                .map(ContractTemplate::getContractTemplatePath)
+                .orElse(null);
+    }
+
+    @Named("toContractTemplateVersion")
+    default String toContractTemplateVersion(Product model, String institutionType){
+        if(Objects.isNull(institutionType)) return null;
+        return Optional.ofNullable(model.getInstitutionContractMappings())
+                .map(map -> map.get(institutionType))
+                .map(ContractTemplate::getContractTemplateVersion)
+                .orElse(null);
     }
 }
