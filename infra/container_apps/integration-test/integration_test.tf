6a2164f60236199a741788335abad45d3b871d38
Giampiero Ferrara <giampiero.ferrara@emeal.nttdata.com>
2025-02-03 10:38:25 +0100

    Fixed resource

--- infra/container_apps/integration-test/integration_test.tf
+++ infra/container_apps/integration-test/integration_test.tf
@@ -21,10 +21,10 @@ resource "github_repository_environment" "repo_environment" {
 resource "github_actions_environment_secret" "integration_environment" {
   repository  = data.github_repository.repo.name
   environment = github_repository_environment.repo_environment.environment
-  secret_name = "integration_environment${local.pnpg_suffix}"
+  secret_name = "integration_environment"
   plaintext_value = base64encode(templatefile("Selfcare-external-Integration.postman_environment.json",
     {
-      env       = local.env_url
+      env       = "${local.env_url}"
       apimKeyPN = data.azurerm_key_vault_secret.apim_product_pn_sk.value
     }))
 }
