6562f7200b2dec8e370a7fa870cd97a29f5f9da3
andrea putzu <andrea.putzu@pagopa.it>
2025-02-05 12:23:28 +0100

    chore: remove unused file

--- integration-test/locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  pnpg_suffix = var.is_pnpg == true ? "-pnpg" : ""
-  project     = "${var.prefix}-${var.env_short}"
-
-  monitor_rg_name          = "${local.project}-monitor-rg"
-  monitor_appinsights_name = "${local.project}-appinsights"
-}
