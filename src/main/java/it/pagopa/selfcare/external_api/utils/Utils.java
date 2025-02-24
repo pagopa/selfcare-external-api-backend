613c5b27db5e2494cd84807b962486fa8958428a
iltommi1995 <tomasdanielavilavisintin@yahoo.it>
2025-02-24 14:04:35 +0100

    [SELC-6455] added tests for utils class

--- src/main/java/it/pagopa/selfcare/external_api/utils/Utils.java
+++ src/main/java/it/pagopa/selfcare/external_api/utils/Utils.java
@@ -8,7 +8,12 @@ import java.util.function.BinaryOperator;
 @UtilityClass
 public class Utils {
     public static BinaryOperator<OnboardedInstitutionInfo> latestInstitutionByCreationDate() {
-        return (existing, replacement) -> existing.getProductInfo().getCreatedAt()
-                .isAfter(replacement.getProductInfo().getCreatedAt()) ? existing : replacement;
+        return (existing, replacement) -> {
+            if(existing == null && replacement == null) throw new NullPointerException();
+            if(existing == null) return replacement;
+            if(replacement == null) return existing;
+            return existing.getProductInfo().getCreatedAt()
+                    .isAfter(replacement.getProductInfo().getCreatedAt()) ? existing : replacement;
+        };
     }
 }
