843506dccc24939e3cd6352d16f67cf5749fbb59
iltommi1995 <tomasdanielavilavisintin@yahoo.it>
2025-02-28 10:00:43 +0100

    [SELC-6455] added private constructor to UserServiceUtils

--- src/main/java/it/pagopa/selfcare/external_api/utils/UserServiceUtils.java
+++ src/main/java/it/pagopa/selfcare/external_api/utils/UserServiceUtils.java
@@ -1,13 +1,14 @@
 package it.pagopa.selfcare.external_api.utils;
 
 import it.pagopa.selfcare.external_api.model.onboarding.OnboardedInstitutionInfo;
-import lombok.experimental.UtilityClass;
 
 import java.util.Objects;
 import java.util.function.BinaryOperator;
 
-@UtilityClass
+
 public class UserServiceUtils {
+    private UserServiceUtils() {}
+
     public static BinaryOperator<OnboardedInstitutionInfo> latestInstitutionByCreationDate() {
         return (existing, replacement) -> {
             if(Objects.isNull(existing) && Objects.isNull(replacement)) throw new NullPointerException();
