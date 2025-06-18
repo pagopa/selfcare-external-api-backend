8820bc1e6e59984453008f78d98d0d5e94fc259f
gianmarcoplutino <119858159+gianmarcoplutino@users.noreply.github.com>
2025-06-18 15:50:16 +0200

    [SELC-7031] feat: added logic to manually onboard an aggregator

---
+++ src/main/java/it/pagopa/selfcare/external_api/model/institution/AggregateInsitution.java
@@ -0,0 +1,31 @@
+package it.pagopa.selfcare.external_api.model.institution;
+
+import it.pagopa.selfcare.external_api.model.onboarding.GeographicTaxonomyDto;
+import it.pagopa.selfcare.external_api.model.onboarding.User;
+import it.pagopa.selfcare.onboarding.common.Origin;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class AggregateInsitution {
+
+    private String taxCode;
+    private String description;
+    private String subunitCode;
+    private String subunitType;
+    private String vatNumber;
+    private String parentDescription;
+    private List<GeographicTaxonomyDto> geographicTaxonomies;
+    private String address;
+    private String zipCode;
+    private String originId;
+    private Origin origin;
+    private List<User> users;
+    private String recipientCode;
+    private String digitalAddress;
+    private String city;
+    private String county;
+    private String iban;
+
+}
---
+++ src/main/java/it/pagopa/selfcare/external_api/model/onboarding/AggregateInstitutionDto.java
@@ -0,0 +1,35 @@
+package it.pagopa.selfcare.external_api.model.onboarding;
+
+import it.pagopa.selfcare.onboarding.common.Origin;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Data
+@SuppressWarnings("java:S1068")
+public class AggregateInstitutionDto {
+    @NotNull(message = "taxCode is required")
+    private String taxCode;
+
+    @NotNull(message = "description is required")
+    private String description;
+
+    private String subunitCode;
+    private String subunitType;
+    private String vatNumber;
+    private String parentDescription;
+    private List<GeographicTaxonomyDto> geographicTaxonomies;
+    private String address;
+    private String zipCode;
+    private String originId;
+    private Origin origin;
+    private List<User> users;
+
+    private String recipientCode;
+    private String digitalAddress;
+    private String city;
+    private String county;
+    private String iban;
+
+}
