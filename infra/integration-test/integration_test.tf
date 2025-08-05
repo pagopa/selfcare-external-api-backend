13cfba85c82849bc64f4a0480f3ddf0a53c86d75
Gaetano Miglionico <gaetano.miglionico@pagopa.it>
2025-08-05 17:24:24 +0200

    [SELC-7508] Bruno collection and workflow (#1046)

--- infra/integration-test/integration_test.tf
+++ infra/integration-test/integration_test.tf
@@ -31,3 +31,14 @@ resource "github_actions_environment_secret" "integration_environment" {
       apimKeyPN = data.azurerm_key_vault_secret.apim_product_pn_sk.value
   }))
 }
+
+resource "github_actions_environment_secret" "integration_environment_bruno" {
+  repository  = data.github_repository.repo.name
+  environment = github_repository_environment.repo_environment.environment
+  secret_name = "integration_environment_bruno${local.pnpg_suffix}"
+  plaintext_value = base64encode(templatefile("Selfcare-External-Integration-Environment.bru",
+    {
+      env       = local.env_url
+      apimKeyPN = data.azurerm_key_vault_secret.apim_product_pn_sk.value
+  }))
+}
