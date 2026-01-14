8b731c53e087c999b80c6bb9c7d74f7ec25790a1
andrea-putzu <106688558+andrea-putzu@users.noreply.github.com>
2026-01-14 15:27:54 +0100

    [SELC-8106] Update product policy

--- infra/apim_v2/apim.tf
+++ infra/apim_v2/apim.tf
@@ -449,25 +449,19 @@ module "apim_external_api_ms_v2" {
     },
     {
       operation_id = "createProduct"
-      xml_content = templatefile("./api/base_ms_url_external_product_policy.xml.tpl", {
-        TENANT_ID              = data.azurerm_client_config.current.tenant_id
-        EXTERNAL-OAUTH2-ISSUER = data.azurerm_key_vault_secret.external-oauth2-issuer.value
+      xml_content = templatefile("./api/base_ms_url_external_policy_post.xml.tpl", {
         MS_BACKEND_URL         = "https://selc-${var.env_short}-product-ms-ca.${var.ca_suffix_dns_private_name}/"
       })
     },
     {
       operation_id = "getProductById"
-      xml_content = templatefile("./api/base_ms_url_external_product_policy.xml.tpl", {
-        TENANT_ID              = data.azurerm_client_config.current.tenant_id
-        EXTERNAL-OAUTH2-ISSUER = data.azurerm_key_vault_secret.external-oauth2-issuer.value
+      xml_content = templatefile("./api/base_ms_url_external_policy_get.xml.tpl", {
         MS_BACKEND_URL         = "https://selc-${var.env_short}-product-ms-ca.${var.ca_suffix_dns_private_name}/"
       })
     },
     {
       operation_id = "patchProductById"
-      xml_content = templatefile("./api/base_ms_url_external_product_policy.xml.tpl", {
-        TENANT_ID              = data.azurerm_client_config.current.tenant_id
-        EXTERNAL-OAUTH2-ISSUER = data.azurerm_key_vault_secret.external-oauth2-issuer.value
+      xml_content = templatefile("./api/base_ms_url_external_policy_put.xml.tpl", {
         MS_BACKEND_URL         = "https://selc-${var.env_short}-product-ms-ca.${var.ca_suffix_dns_private_name}/"
       })
     },
