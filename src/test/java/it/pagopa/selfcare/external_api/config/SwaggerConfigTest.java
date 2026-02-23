a082a006b76d14545b259fe68733a62a511177f7
Gaetano Miglionico <gaetano.miglionico@pagopa.it>
2026-02-23 15:02:10 +0100

    [SELC-8116] New SwaggerConfig with springdoc

--- src/test/java/it/pagopa/selfcare/external_api/config/SwaggerConfigTest.java
+++ src/test/java/it/pagopa/selfcare/external_api/config/SwaggerConfigTest.java
@@ -8,16 +8,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -28,14 +24,7 @@ import java.util.regex.Pattern;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest(classes = {
-        SwaggerConfig.class,
-        WebConfig.class
-})
-@EnableOpenApi
-@EnableWebMvc
-@ComponentScan(basePackages = {"it.pagopa.selfcare.external_api.controller","it.pagopa.selfcare.external_api.model" })
-@TestPropertySource(locations = "classpath:config/application.yml")
+@SpringBootTest
 @Slf4j
 class SwaggerConfigTest {
 
