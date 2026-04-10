5984dfa28d6a73cc40a2cffd75de2b34e7f82e41
fabiosalamonenttdata <fabio.salamone@nttdata.com>
2026-04-10 17:44:38 +0200

    [8468] feat: Add Document API client and update related configurations

--- src/main/java/it/pagopa/selfcare/external_api/client/MsDocumentApiClient.java
+++ src/main/java/it/pagopa/selfcare/external_api/client/MsDocumentApiClient.java
@@ -3,6 +3,5 @@ package it.pagopa.selfcare.external_api.client;
 import it.pagopa.selfcare.document.generated.openapi.v1.api.DocumentControllerApi;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "${rest-client.ms-document-api.serviceCode}", url = "${rest-client.ms-document.base-url}")
 public interface MsDocumentApiClient extends DocumentControllerApi {
 }
