e0dcaff575e3e1139fd553ec589c98c03cdae140
fabiosalamonenttdata <fabio.salamone@nttdata.com>
2026-04-13 09:02:13 +0200

    feat: add missing feign config to MsDocumentApiClient

--- src/main/java/it/pagopa/selfcare/external_api/client/MsDocumentApiClient.java
+++ src/main/java/it/pagopa/selfcare/external_api/client/MsDocumentApiClient.java
@@ -1,7 +1,13 @@
 package it.pagopa.selfcare.external_api.client;
 
 import it.pagopa.selfcare.document.generated.openapi.v1.api.DocumentControllerApi;
+import it.pagopa.selfcare.external_api.client.config.MsDocumentApiClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 
+@FeignClient(
+        name = "${rest-client.ms-document-api.serviceCode}",
+        url = "${rest-client.ms-document.base-url}",
+        configuration = MsDocumentApiClientConfig.class
+)
 public interface MsDocumentApiClient extends DocumentControllerApi {
 }
